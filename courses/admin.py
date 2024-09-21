bce6fb8e0a770d83a060d6f0e3d5397c329ee91a
Choi9912 <vkvkdl23@naver.com>
2024-09-21 14:47:27 +0900

    코스에 대한 admin 생성 및 구현

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Course, Lesson
 
-# Register your models here.
+
+class LessonInline(admin.TabularInline):
+    model = Lesson
+    extra = 1
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "order")
+    list_filter = ("order",)
+    search_fields = ("name", "description")
+    inlines = [LessonInline]
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("title", "course", "video_url", "order")
+    list_filter = ("course", "order")
+    search_fields = ("title", "course__name")
