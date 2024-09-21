4956f681337a3544284b3312a2c1a8a12e741a18
Choi9912 <vkvkdl23@naver.com>
2024-09-21 14:41:44 +0900

    증명서 admin 생성 및 구현

--- certificates/admin.py
+++ certificates/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Certificate
 
-# Register your models here.
+
+@admin.register(Certificate)
+class CertificateAdmin(admin.ModelAdmin):
+    list_display = ("user", "course", "issue_date", "verification_code")
+    list_filter = ("issue_date", "course")
+    search_fields = ("user__username", "course__name", "verification_code")
+    readonly_fields = ("issue_date", "verification_code")
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # 이미 생성된 객체인 경우
+            return self.readonly_fields + ("user", "course")
+        return self.readonly_fields
