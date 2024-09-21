02c7cd5d0585f74528d39355cb5934399bdb5725
Choi9912 <vkvkdl23@naver.com>
2024-09-21 14:42:37 +0900

    verification_code 필드와 is_expired(1년 후 만료)

--- certificates/models.py
+++ certificates/models.py
@@ -1,11 +1,16 @@
-# certificates/models.py
 from django.db import models
 from django.conf import settings
 from courses.models import Course
+from django.utils import timezone
 
 
 class Certificate(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     issue_date = models.DateTimeField(auto_now_add=True)
-    pdf_url = models.URLField()
+    pdf_url = models.URLField(blank=True)
+    verification_code = models.CharField(max_length=50, unique=True)
+
+    def is_expired(self):
+        # 예: 1년 후 만료
+        return timezone.now() > (self.issue_date + timezone.timedelta(days=365))
