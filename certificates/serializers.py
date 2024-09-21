ba075bb0826257bce06a0a44fce23de10a9bcc9b
Choi9912 <vkvkdl23@naver.com>
2024-09-21 14:44:50 +0900

    ModelSerializer를 상속, 추가필드정의 , 유효성 검사 메서드

--- certificates/serializers.py
+++ certificates/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 from .models import Certificate
+from courses.models import Course
 
 
 class CertificateSerializer(serializers.ModelSerializer):
+    course_name = serializers.CharField(source="course.name", read_only=True)
+
     class Meta:
         model = Certificate
         fields = "__all__"
-        read_only_fields = ["issue_date", "pdf_url"]
+        read_only_fields = ["issue_date", "pdf_url", "verification_code", "course_name"]
+
+    def validate_course(self, value):
+        if not Course.objects.filter(id=value.id).exists():
+            raise serializers.ValidationError("Invalid course selected.")
+        return value
